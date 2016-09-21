22b6ca176a51ddef5fb05d6bac461611a72b9a08
Jun Quan Tham <junquantham@Juns-iMac.local>
2016-09-22 00:09:07 +0800

    added 00_read_data

---
+++ code/00_read_data.py
@@ -0,0 +1,58 @@
+# python3.5.2
+
+from datetime import datetime
+import os, sys, traceback
+
+# Utility class for timing the script
+# --------------------------------------
+class Timer:
+    def __init__(self):
+        pass
+
+    def getTimeString(self, mydate):
+        return str(mydate.strftime('%Y-%m-%d %H:%M:%S'))
+
+    def getTime(self):
+        return datetime.now()
+
+print('------------------------------------------------------------')
+print('Reading files ... ')
+timer = Timer()
+startTime = timer.getTime()
+print('------------------------------------------------------------')
+
+
+# Parameters
+inputDir  = '../data/protocol/'
+outputDir = 'data/'
+
+# Start Code
+try:
+    timer = Timer()
+    print('Start Time : ', timer.getTime())  # Get the start time for tracking purposes
+
+    for subdir, dirs, files in os.walk(inputDir):  # Walk through each file in the dir
+        for file in files:
+
+            inputFilePath = os.path.join(subdir, file)
+            inputFileName = os.path.splitext(file)[0]
+            inputFileExt = os.path.splitext(file)[1]
+
+            print('Reading data file : ', inputFilePath)  # Opening file ...
+            
+except:
+    error_type = sys.exc_info()[0]
+    error_message = sys.exc_info()[1]
+    error_line = sys.exc_info()[2].tb_lineno
+
+    print('******************************************************')
+    print('traceback information')
+    print('******************************************************')
+    traceback.print_tb(sys.exc_info()[2])
+    print('******************************************************')
+    print('error information')
+    print('******************************************************')
+    print(("Error | %s - %s | Line %s" % (error_type, error_message, error_line)))
+    print('******************************************************')
+
+
---
+++ references/useful_links.md
@@ -0,0 +1,5 @@
+# Useful Links
+
+* Git Workflow [here] (https://www.atlassian.com/git/tutorials/comparing-workflows/centralized-workflow)
+* We are probably following the centralized workflow. 
+
