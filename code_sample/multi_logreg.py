82619a6c224ecc86f00ae2012a1f089db5911d57
Thomas Jiang <thomasjiangcy@gmail.com>
2016-11-16 00:01:08 +0800

    update multi_logreg

--- code_sample/multi_logreg.py
+++ code_sample/multi_logreg.py
@@ -1,40 +1,6 @@
 from datetime import datetime
 import pandas as pd
-# Define Errors
-class Error(Exception):
-    '''Base class for exceptions'''
-    pass
-
-
-class InputError(Error):
-    '''Exception raised for errors in target argument'''
-    def __init__(self, message):
-        self.message = message
-
-
-# Define methodology class
-class MethodClass(object):
-    
-    def __init__(self):
-        pass
-    
-    
-    def init_class(self, train_data, target):
-        '''
-        Method for choosing first target variable.
-        A -> B
-        where A is subject and B is activity_id 
-        or vice-versa.
-        '''   
-        target_variables = ['subject', 'activity_id']
-        
-        if target in target_variables:
-            x_train = train_data.drop(target_variables, axis=1)  # Exclude 'activity_id', 'subject' from input variables
-            y_train = train_data[target]  # assign training target
-            return [x_train, y_train]
-        else:
-            message = 'Invalid target variable'
-            raise InputError(message)
+from methods import MethodClass
 
 # Load data
 print('Loading data via "train_data.txt", "test_data.txt"...')
