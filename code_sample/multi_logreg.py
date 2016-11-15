a51b059ad5034f7ee2815691421b471e72b526f2
thomasjiangcy <thomasjiangcy@gmail.com>
2016-11-15 20:15:43 +0800

    updated multi_logreg.py

--- code_sample/multi_logreg.py
+++ code_sample/multi_logreg.py
@@ -1,64 +1,37 @@
-import numpy as np
-import pandas as pd
 from datetime import datetime
+import pandas as pd
 
 
 # Load data
-print('Loading data via "consolidated.txt"...')
+print('Loading data via "train_data.txt", "test_data.txt"...')
 start_time = datetime.now()
-data = pd.read_csv('../data/consolidated.txt', sep=',')
+train_data = pd.read_csv('../data/train_data.txt', sep=',')
+test_data = pd.read_csv('../data/test_data.txt', sep=',')
 end_time = datetime.now()
 duration = end_time - start_time
-print('Date file loaded.')
+print('Date files loaded.')
 print('Time taken: ', duration)
 print('--------------------------------------------------------------------')
-
-
-# Stratified Shuffle Split
-print('Stratified splitting in progress...')
-start_time = datetime.now()
-
-from sklearn.model_selection import StratifiedShuffleSplit
-
-random_state = np.random.seed(2016) # Set random seed
-
-# Instantiate Stratified Shuffle Split
-strat_split = StratifiedShuffleSplit(n_splits=1,
-                                     train_size=0.75,
-                                     random_state=random_state)
-x = data.ix[:,:53] # Set input variables
-y = data.ix[:, 54] # Set output variable to be 'subject'
-
-for train_index, test_index in strat_split.split(x,y):
-    print('TRAIN indices: ', train_index)
-    print('TEST indices: ', test_index)
-    x_train, x_test = x.ix[train_index], x.ix[test_index]
-    y_train, y_test = y.ix[train_index], y.ix[test_index]
-
-end_time = datetime.now()
-duration = end_time - start_time
-print('Train and test data stratified splitting complete.')
-print('x_train shape: ', x_train.shape)
-print('y_train shape: ', y_train.shape)
-print('x_test shape: ', x_test.shape)
-print('y_test shape: ', y_test.shape)
-print('Time taken: ', duration)
-print('--------------------------------------------------------------------')
-
 
 # Multinomial Logistic Regression
 from sklearn.linear_model import LogisticRegression
-from sklearn import metrics
+from sklearn.model_selection import GridSearchCV
 
+print('Multinomial Logistic Regressions over c = 0.01, 0.1, 1, 10, 100')
+print('--------------------------------------------------------------------')
+print('Execute GridSearchCV with cv=10...')
+start_time = datetime.now()
 c = [0.01, 0.1, 1, 10, 100]
+parameters = [
+    {'C': c}
+]
 
-for i in range(len(c)):
-    lr = LogisticRegression(multi_class = 'multinomial',
-                            solver = 'newton-cg',
-                            C = c[i])    
-    lr.fit(x_train, y_train)
-    y_pred = lr.predict(x_test)
-    accuracy = metrics.accuracy_score(y_test, y_pred)
-    print('At C: %.2f' % c[i])
-    print('Accuracy of LR: ', accuracy)
-    print('--------------------')
+lr_best = GridSearchCV(LogisticRegression(multi_class='multinomial',solver='newton-cg'), parameters, cv=10)
+end_time = datetime.now()
+duration = end_time - start_time
+print('GridSearchCV complete.')
+print('Time taken: ', duration)
+print('--------------------------------------------------------------------')
+print('The best parameters selected: ', lr_best.best_params_)
+print('Best score produced with parameters: ', lr_best.best_score_)
+print('--------------------------------------------------------------------')
