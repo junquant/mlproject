271fe8b24307f2b94299521fd586eac791ece077
Thomas Jiang <thomasjiangcy@gmail.com>
2016-11-28 23:05:53 +0800

    added scaling

--- code/07_final_model_kfold.py
+++ code/07_final_model_kfold.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 import time
 from sklearn.model_selection import StratifiedKFold
-from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import classification_report, accuracy_score
+from sklearn.preprocessing import scale
 
 from utilities import Timer, MetaData, ResultsWriter
 
@@ -37,6 +38,9 @@ df.rename(columns={0:'activity_subj'}, inplace=True)
 skf = StratifiedKFold(n_splits=3, shuffle=False, random_state=2016)
 readings = df.ix[:,:-3]
 
+# Scale data
+readings = scale(readings)
+
 # K-Fold split based on subj_activity
 for train_index, test_index in skf.split(readings,subj_activity):
     df_train, df_test = df.ix[train_index], df.ix[test_index]
@@ -61,7 +65,7 @@ print(verify)
 readings_train = df_train.ix[:,:-3]
 subj_activity_train = df_train.ix[:,-1]
 
-# step 1.3 - fit the model to predict subject
+# step 1.2 - fit the model to predict subject
 print('Fitting model to predict subject ...')
 clf_both = GaussianNB()
 time_bgn = time.time()
@@ -71,9 +75,8 @@ dur_train_both = time.time() - time_bgn
 # step 2.1 - get the readings data (from data stratified using subject)
 readings_test = df_test.ix[:,:-3]
 
-# step 2.3 - predict subject activity
+# step 2.2 - predict subject activity
 print('Predicting subject activity ... ')
-readings_test = rbf_feature.fit_transform(readings_test)
 predicted_subj_activity = clf_both.predict(readings_test)
 
 # step 3 - printing results
@@ -82,7 +85,7 @@ actual_subj_activity = df_test.ix[:,-1]
 print(classification_report(actual_subj_activity, predicted_subj_activity))
 print('accuracy score: ', accuracy_score(actual_subj_activity, predicted_subj_activity))
 
-ResultsWriter.write_to_file('results_thomas.txt',model='pca_gnb',
-                            y_actual=actual_subj_activity,y_predicted=predicted_subj_activity,
-                            dur_train_activity=0, dur_train_subj=0, dur_train_both=dur_train_both,
-                            method='both')
+#ResultsWriter.write_to_file('results_thomas.txt',model='pca_gnb',
+#                            y_actual=actual_subj_activity,y_predicted=predicted_subj_activity,
+#                            dur_train_activity=0, dur_train_subj=0, dur_train_both=dur_train_both,
+#                            method='both')
